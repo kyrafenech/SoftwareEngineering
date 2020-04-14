f60277e07cf1d91a2c8a72569263f1817b2bc9ed
kyrafenech <kyra.fenech.18@um.edu.mt>
2020-04-14 18:09:41 +0200

    Move player function & testing

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,13 +1,40 @@
 public class Player {
 
-    Position position;
+    Position current;
+    Position initial;
 
+    public Player() {
+        this.initial = new Position(0,0); //this will be randomly generated
+        this.current= new Position(0,0); //this will start off as initial
+    }
     public void move(char direction){
-        
+        int X, Y; //new coordinates
+        switch (direction){
+            case 'U':
+                X= this.current.getX(); //x-coordinate stays the same
+                Y=this.current.getY() +1; //y-coordinate moves up by 1
+                break;
+            case 'D':
+                X= this.current.getX(); //x-coordinate stays the same
+                Y=this.current.getY() -1; //y-coordinate moves down by 1
+                break;
+            case 'R':
+                X= this.current.getX() +1; //x-coordinate moves right by 1
+                Y=this.current.getY(); //y-coordinate stays the same
+                break;
+            case 'L':
+                X= this.current.getX()-1; //x-coordinate moves left by 1
+                Y=this.current.getY() ; //y-coordinate stays the same
+                break;
+            default:
+                //in the case of an invalid token the player does not move
+                X= this.current.getX();
+                Y=this.current.getY() ;
+        }
+        this.current.setPosition(X, Y);
+
+
     }
 
-    public boolean setPosition(Position p ){
 
-        return true;
-    }
 }
--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -2,4 +2,23 @@ public class Position {
 
     int x;
     int y;
+
+    public Position(int x, int y) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean setPosition(int X, int Y) {
+        this.x = X;
+        this.y = Y;
+        return true;
+    }
 }
