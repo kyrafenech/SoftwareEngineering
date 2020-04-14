aaa18f3ab954230a8a37ca92ed82f9b730dcc568
kyrafenech <kyra.fenech.18@um.edu.mt>
2020-04-14 16:17:52 +0200

    Position class constructors

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,13 +1,18 @@
 public class Player {
 
-    Position position;
+    Position current;
+    Position initial;
+
+    public Player(Position current, Position initial) {
+        this.initial = initial; // randomly generated
+        this.current = initial; //initially set to first position
+
+    }
 
     public void move(char direction){
-        
+
+
+
     }
 
-    public boolean setPosition(Position p ){
-
-        return true;
-    }
 }
