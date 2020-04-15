c89f44bbf117f82c94a76553815d4298257154a2
mariahzm <mariah.zammit.18@um.edu.mt>
2020-04-15 10:23:13 +0200

    method to check if move is legal

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -2,4 +2,14 @@ public class Position {
 
     int x;
     int y;
+
+    //Ensuring that the user does not try to move outside the map
+    private boolean isLegal(int n){
+        if(x <= n && y <= n){
+            return true;
+        }
+
+        return false;
+    }
+
 }
