c96dd63aee33977d566b829dd07c3f0c9b7f5d80
mariahzm <mariah.zammit.18@um.edu.mt>
2020-04-15 10:36:18 +0200

    no changes

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -3,10 +3,6 @@ public class Position {
     int x;
     int y;
 
-<<<<<<< HEAD
-
-}
-=======
     public Position(int x, int y) {
         this.y = y;
         this.x = x;
@@ -25,5 +21,6 @@ public class Position {
         this.y = Y;
         return true;
     }
+
 }
->>>>>>> f60277e07cf1d91a2c8a72569263f1817b2bc9ed
+
