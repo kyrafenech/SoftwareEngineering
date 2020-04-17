f2bb5ee2f7005d8f96b6bc9f291bf8b6e36630b8
kyrafenech <kyra.fenech.18@um.edu.mt>
2020-04-17 22:57:50 +0200

    Generate tiles & grid

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Map {
 
     int size;
+    Tile[][] grid;
 
 
     public boolean setMapSize(int x, int y, int n){
@@ -29,10 +30,47 @@ public class Map {
 
     }
 
-    public void generate(){
+    public Tile[][] generate(){
+        this.grid = new Tile[size][size];
+        Random rand = new Random();
+        setTreasure(rand); //treasure tile is chosen first
+        setWaterTile(rand);
+        setGrass();
+        return this.grid;
+   }
 
+    public void setTreasure(Random random){
+        int random_x = random.nextInt(this.size);
+        int random_y = random.nextInt(this.size);
+
+        this.grid[random_x][random_y] = new TreasureTile();
     }
 
+    public void setWaterTile(Random random){
+        //number of water tiles will be approximately 30% of the size
+        int amount = (int)((float)Math.pow(this.size,2)/3);
+        int i;
+        for(i =0; i < amount; i++){
+            int random_x = random.nextInt(this.size);
+            int random_y = random.nextInt(this.size);
+            //in case the random coordinates are occupied by another tile
+            if(this.grid[random_x][random_y] != null){
+                amount++;
+                continue;
+            }else{
+                this.grid[random_x][random_y] = new WaterTile();
+            }
+        }
+    }
+
+    public void setGrass(){
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                if (this.grid[i][j] == null)
+                    this.grid[i][j] = new GrassTile();
+            }
+        }
+    }
 
     public char getTileType(int x, int y){
 
