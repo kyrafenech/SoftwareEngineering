b6f40781c813f3a2f77c1c7072a63a5e1ca299d7
mariahzm <mariah.zammit.18@um.edu.mt>
2020-04-19 22:30:06 +0200

    added comments

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -11,9 +11,9 @@ public class Game {
     private Player[] players;
     private Map map;
     private File[] htmlFiles;
-    private BufferedWriter[] bw;
+    private BufferedWriter[] bw; //will allow us to write to files
 
-    private boolean setNumPlayers(int playerCount) {
+    public boolean setNumPlayers(int playerCount) {
         int minPlayers = 2;
         int maxPlayers = 8;
 
@@ -25,7 +25,7 @@ public class Game {
         return true;
     }
 
-    private boolean setMapSize(int size) {
+    public boolean setMapSize(int size) {
         final int MAX = 50;
         //setting minimum number of map size according to amount of players
         final int MIN = players.length <= 4 ? 5 : 8;
@@ -42,33 +42,45 @@ public class Game {
     }
 
     public void generateHTMLFiles() throws IOException {
+        //variables for loops
         int i, j, k;
 
+        //contains the html code to be written to file
         StringBuilder html = new StringBuilder();
 
+        //this method will only execute on the initial generation
         if (htmlFiles == null) {
             htmlFiles = new File[players.length];
             bw = new BufferedWriter[players.length];
             for (i = 0; i < htmlFiles.length; i++) {
+                //creating the file for the player
                 htmlFiles[i] = new File("C:\\map_player_" + (i + 1) + ".html");
                 bw[i] = new BufferedWriter(new FileWriter(htmlFiles[i]));
             }
         }
 
+        //loop for each player
         for (i = 0; i < htmlFiles.length; i++) {
             html.append("<html><head><title>table</title><style>" +
                     "table{ border-collapse: collapse; width: 50%; } " +
                     "td { border: 2 px solid black; padding: 15 px; }" +
                     "</style></head><body><table>");
 
+            //nested-loop to parse through each tile in grid and writing html code accordingly
             for (j = 0; j < map.getSize(); j++) {
                 html.append("<tr>");
                 for (k = 0; k < map.getSize(); k++) {
+                    /*if the tile is uncovered then we get corresponding html code
+                    otherwise, tile is greyed out
+                     */
                     if (players[i].getMap().getTile(j, k).getUncovered()) {
                         html.append(players[i].getMap().getTile(j, k).getHtml());
                     } else {
                         html.append("<td bgcolour=#808080>");
                     }
+                    /* if the tile coordinates correspond to the player's current position
+                    the cell will contain an '*' to indicate the player's position
+                     */
                     if (players[i].getCurrent() == new Position(j, k)) {
                         html.append("*");
                     }
@@ -79,10 +91,10 @@ public class Game {
 
             html.append("</table></body></html>");
 
+            //writing the code to the corresponding file
             bw[i].write(html.toString());
             bw[i].close();
         }
-
     }
 
     public static void main(String[] args) throws IOException {
