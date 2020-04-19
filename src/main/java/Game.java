3a7dcf3c81eb867fdfaa74d99998cf9169abf840
mariahzm <mariah.zammit.18@um.edu.mt>
2020-04-19 15:41:27 +0200

    added coments and fixed setNumPlayers syntax

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,25 +5,20 @@ public class Game {
     Map map;
 
 
-    public boolean setNumPlayers(int n){
+    public boolean setNumPlayers(int playerCount){
+        int minPlayers = 2;
+        int maxPlayers = 8;
 
-        int min_players = 2;
-        int max_players = 8;
-        if(n<min_players){
-            return false;
-        }
-        else if(n>max_players){
-            return false;
-        }else {
-            return true;
-        }
+        //validating given number of players [2 < n < 8]
+        return playerCount < minPlayers || playerCount > maxPlayers ? false : true;
     }
 
     private boolean setMapSize(int size){
-
         final int MAX = 50;
+        //setting minimum number of map size according to amount of players
         final int MIN = players.length <= 4 ? 5 : 8;
 
+        //validating given size
         return MIN <= size && size <= MAX ? true : false;
     }
 
